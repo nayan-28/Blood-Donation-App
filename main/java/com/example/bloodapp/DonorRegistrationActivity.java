fe4dbd93e872a2cfc730c6fcd72711aa06e9a90e
nayan-28 <nayanmalakar720@gmail.com>
2022-08-13 00:36:28 +0600

    Source

---
+++ main/java/com/example/bloodapp/DonorRegistrationActivity.java
@@ -0,0 +1,28 @@
+package com.example.bloodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class DonorRegistrationActivity extends AppCompatActivity {
+
+    private TextView backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_registration);
+
+        backButton=findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DonorRegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ main/java/com/example/bloodapp/RecipientRegistrationActivity.java
@@ -0,0 +1,28 @@
+package com.example.bloodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class RecipientRegistrationActivity extends AppCompatActivity {
+
+    private TextView backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipient_registration);
+
+        backButton=findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecipientRegistrationActivity.this, LoginActivity.class);
+            }
+        });
+    }
+}
